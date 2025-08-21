7486f8c6169e72a7b4c58338227e88a45c86b61c
plunkydev <plunkydesign@gmail.com>
2025-08-21 15:47:30 -0500

    feat: añadir clases Node y LinkedList con método append y pruebas iniciales

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
 import './styles.css';
 
-console.log('proyect-linked-list-odin');
+class Node {
+  constructor(value) {
+    this.node = value;
+    this.nextNode = null;
+  }
+}
 
-// Tu código aquí...
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      let current = this.head;
+      let father = current;
+      while (current) {
+        father = current;
+        current = current.nextNode;
+      }
+      current = newNode;
+      father.nextNode = current;
+      this.tail = newNode;
+    }
+    this.size++;
+  }
+}
+
+const list = new LinkedList();
+list.append('dog');
+list.append('cat');
+list.append('parrot');
+list.append('hamster');
+list.append('snake');
+list.append('turtle');
+
+console.log(list);
